f39329c23cce9a0e57f9cb3464720f071135745c
twitter <alikeashe5@gmail.com>
2024-03-16 14:01:23 +0200

    version1

--- app/src/main/java/com/example/sec2_app3/MainActivity.java
+++ app/src/main/java/com/example/sec2_app3/MainActivity.java
@@ -1,26 +1,58 @@
 package com.example.sec2_app3;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Spinner spnMenu;
+    private Button btnSearch;
+    private ListView lstItems;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        //TODO: fill this feature
-        //This is another comment
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        setupViews();
+
+        populateSpinner();
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseItems db = new DatabaseItems();
+                String cat = spnMenu.getSelectedItem().toString();
+
+                List<MenuItem> result = db.getMenuItems(cat);
+                MenuItem[] arr = result.toArray(new MenuItem[result.size()]);
+                ArrayAdapter<MenuItem> adapter = new ArrayAdapter<MenuItem>(MainActivity.this,
+                        android.R.layout.simple_list_item_1, arr);
+                lstItems.setAdapter(adapter);
+
+
+            }
         });
+
+    }
+
+    private void populateSpinner() {
+        DatabaseItems db = new DatabaseItems();
+        String[] cats = db.getCategories();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, cats);
+        spnMenu.setAdapter(adapter);
+    }
+
+    private void setupViews() {
+        spnMenu = findViewById(R.id.spnMenu);
+        btnSearch = findViewById(R.id.btnSearch);
+        lstItems = findViewById(R.id.lstItems);
     }
 }
